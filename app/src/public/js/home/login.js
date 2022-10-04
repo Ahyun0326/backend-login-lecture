297eaf01945ac2e83b354ce7cf296f74c544f7aa
YeSeo <chinkl0110@naver.com>
2022-10-04 21:36:40 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),    //태그 내 id가 id인 것과 id가 psword인 것 가져오겠다
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+//로그인 버튼을 눌러 클릭이라는 이벤트가 발생했을 때 login 함수 실행
+loginBtn.addEventListener("click", login);
+
+function login(){
+    //console.log(id.value);  //태그의 값을 가져와 console 창에 출력
+    //아이디와 비밀번호 가져와 객체 형태로 저장
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+}
