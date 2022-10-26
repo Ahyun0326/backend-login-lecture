01a6ff69040666ecbeffade27aaa6d70f53107e3
YeSeo <chinkl0110@naver.com>
2022-10-27 00:25:52 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,23 +1,26 @@
 "use strict";
 
 const id = document.querySelector("#id"),    //태그 내 id가 id인 것과 id가 psword인 것 가져오겠다
-psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("#button");
+    name = document.querySelector("#name"),
+    psword = document.querySelector("#psword"),
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
+//sign up 버튼을 눌러 클릭이라는 이벤트가 발생했을 때 register 함수 실행
+registerBtn.addEventListener("click", register);
 
-//로그인 버튼을 눌러 클릭이라는 이벤트가 발생했을 때 login 함수 실행
-loginBtn.addEventListener("click", login);
-
-function login(){
+function register(){
     //console.log(id.value);  //태그의 값을 가져와 console 창에 출력
     //아이디와 비밀번호 가져와 객체 형태로 저장
     const req = {
         id : id.value,
+        name : name.value,
+        confirmPsword : confirmPsword.value,        
         psword : psword.value,
     };
-    //console.log(req);
+    //console.log(req); 값이 잘 담겼는지 확인
     //console.log(JSON.stringify(req));
 
-    fetch("/login", {
+    fetch("/register", {
         method : "POST",
         //이러한 타입으로 JSON 데이터를 전송하겠다.
         headers: {
@@ -28,12 +31,12 @@ function login(){
      .then((res) => res.json())
      .then((res) => {
         if(res.success){
-            location.href="/";
+            location.href="/login";
         } else{
             alert(res.msg);
         }
-    }) 
+    })
     .catch((err) => {
-        console.log("로그인 중 에러 발생");
+        console.log("회원가입 중 에러 발생");
     });
 }
